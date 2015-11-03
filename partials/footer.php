14fba52b586636f2c9e5addbecfc9e340dc6d8c2
Remi <Remi@MacBook-Pro-de-Remi.local>
2015-11-03 15:49:21 +0100

    Init

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+ <div class="footer-container">
+    <footer class="wrapper clearfix">
+        <ul>
+            <li><a href="/">Home</a></li>
+        	<li><a href="mailto:contact@tilk-theapp.com">Contact</a></li>
+        	<li><a href="https://www.facebook.com/TilkTheApp?ref=br_tf" target="_new">Facebook</a></li>
+        	<li><a href="https://twitter.com/TilkTheApp" target="_new">Twitter</a></li>
+        	<li><a href="mailto:support@tilk-theapp.com">Support</a></li>
+        	<li><a href="privacy.php">Sécurité et Confidentialité</a></li>
+        </ul> 
+    </footer>
+</div>
